48b4804717dbea44f46bae4455d0b72cd68fc203
Kimihiro Hattori <hattori.kimihiro@g.sp.m.is.nagoya-u.ac.jp>
2023-04-27 21:47:31 +0900

    完成

--- ex1/k_hattori/main.py
+++ ex1/k_hattori/main.py
@@ -1,63 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import wave
+import soundfile as sf
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+def wavload(path):
+    data, samplerate = sf.read(path)
+    return data, samplerate
 
 def main():
     # wavファイルの読み込み
-    wavf = 'ONSEI.wav'
-    wr = wave.open(wavf, 'r')
-    ch = wr.getnchannels()  # チャンネル数
-    samplesize = wr.getsampwidth()   # サンプルサイズ
-    fr = wr.getframerate()  # サンプリングレート
+    path = 'ONSEI.wav'
+    data, samplerate = wavload(path)
     fn = 436000    # データ整形のために指定
-    data = wr.readframes(fn)    # fn個のフレームを読んでbytesオブジェクトで返す
-    wr.close()
-    # 数値に変換
-    if samplesize == 2:
-        num_data = np.frombuffer(data, dtype = 'int16')
-    elif samplesize == 4:
-        num_data = np.frombuffer(data, dtype = 'int32')
     # データを扱いやすく整形
-    num_data = num_data[:fn]/float((2^15))
-    TOTAL_TIME = 1.0 * fn / fr
-
-    time = np.linspace(0, TOTAL_TIME, num_data.shape[0])
-    sampling_rate = fr
-
-    print('チャンネル', ch)
-    print('総フレーム数', fn)
-    print('再生時間', TOTAL_TIME)
-    print(num_data)
-
-    plt.plot(time, num_data)
-    plt.xlabel("time [s]")
-    plt.ylabel("Amplitude")
-    plt.show()
+    data = data[:fn]
+    time = np.arange(0, len(data))/samplerate
+    TOTAL_TIME = len(data)/samplerate
 
     WIDTH = 1000    # 分割フレームの大きさ
     OVERLAP = int(WIDTH / 2) # フレームのシフト幅
 
-    wave_length = num_data.shape[0]    # 音声の全フレーム数
+    wave_length = data.shape[0]    # 音声の全フレーム数
     split_number = len(np.arange((WIDTH/2), wave_length, (WIDTH - OVERLAP))) # 音声の分割数
-    unit_time = TOTAL_TIME * WIDTH / fn # フレーム時間分解能
-    unit_freq = 1 / unit_time   # フレーム周波数分解能
-
-    print(unit_time, unit_freq)
 
     window = np.hamming(WIDTH)
     pos = 0
 
     # 分割でフーリエ変換したデータのサイズを求める
-    fframe_size = np.fft.fft(num_data[pos:pos+WIDTH])
+    fframe_size = np.fft.fft(data[pos:pos+WIDTH])
     fframe_size = fframe_size.shape[0]
-    # スペクトログラムの配列
+    # STFTの格納配列
     spec = np.zeros([split_number, fframe_size], dtype=complex)
-    im_result = spec    # 虚部の確保をする配列
 
     # STFT
     for i in range(split_number):
-        frame = num_data[pos:pos+WIDTH]
+        frame = data[pos:pos + WIDTH]
         windowed = window * frame   # 窓関数をかける
         # 短時間に分けたものをフーリエ変換
         fft_result = np.fft.fft(windowed)
@@ -69,15 +46,7 @@ def main():
     amp = np.real(spec[:,int(spec.shape[1]/2)::-1])
     amp = np.log(amp** 2)
 
-    # スペクトログラムの表示
-    plt.rcParams['image.cmap'] = 'jet'
-    plt.imshow(amp.T, extent=[0, TOTAL_TIME, 0, sampling_rate/2000], aspect="auto")
-    plt.xlabel("time [s]")
-    plt.ylabel("frequency [kHz]")
-    plt.colorbar()
-    plt.show()
-
-    ifft_wave = np.zeros(num_data.shape)
+    ifft_wave = np.zeros(data.shape)
     pos = 0
     # iSTFT
     for i in range(split_number):
@@ -87,10 +56,49 @@ def main():
         
         pos += OVERLAP
 
-    plt.plot(time, ifft_wave)
+    
+    fig = plt.figure(figsize=(6,6))
+    plt.rcParams["image.cmap"] = "jet"
+    plt.rcParams["font.family"] = "Times New Roman"
+    plt.rcParams["font.size"] = 12
+
+    ax1 = fig.add_subplot(311)
+    ax1.set_title("Original signal")
+    ax1.plot(time, data)
+    ax1.set_ylabel("Magnitude")
+    ax1.set_ylim(-1,1)
+    ax1.set_xlim(0,TOTAL_TIME)
+
+    ax2 = fig.add_subplot(312)
+    ax2.set_title("Spectrogram")
+    im = ax2.imshow(amp.T, extent=[0, TOTAL_TIME, 0, samplerate/2000], aspect='auto')
+    divider1 = make_axes_locatable(ax2)
+    cax1 = divider1.append_axes("right", size="2%", pad=0.1)
+    cbar = plt.colorbar(im, cax=cax1)
+    ax2.set_ylabel("Frequency [kHz]")
+    ax2.set_xlim(0, TOTAL_TIME)
+    ax2.set_ylim(0, samplerate/2000)
+
+    ax3 = fig.add_subplot(313)
+    ax3.set_title("Re-synthesized signal")
+    ax3.plot(time, ifft_wave)
+    ax3.set_ylabel("Magnitude")
+    ax3.set_xlabel("Time [s]")
+    ax3.set_ylim(-1,1)
+    ax3.set_xlim(0,TOTAL_TIME)
+
+    fig.tight_layout()
+
+    # グラフの横幅を揃える
+    fig.canvas.draw()
+    axpos1 = ax1.get_position() # 一段目の図の描画領域
+    axpos2 = ax2.get_position() # 二段目の図の描画領域
+    axpos3 = ax3.get_position() # 三段目の図の描画領域
+    #ax1とax3の幅をax2と同じにする
+    ax1.set_position([axpos1.x0, axpos1.y0, axpos2.width, axpos1.height])
+    ax3.set_position([axpos3.x0, axpos3.y0, axpos2.width, axpos3.height])
+
     plt.show()
-
-
 
 
 if __name__ == '__main__':
