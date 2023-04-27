77807760d1a2b82c4957e13cb64f585e9b50cdd2
Kimihiro Hattori <hattori.kimihiro@g.sp.m.is.nagoya-u.ac.jp>
2023-04-27 22:06:25 +0900

    完成

--- ex1/k_hattori/main.py
+++ ex1/k_hattori/main.py
@@ -3,34 +3,38 @@ import matplotlib.pyplot as plt
 import soundfile as sf
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+
 def wavload(path):
+    """"引数のpathから音声を読み込み，データとサンプリングレートを返す"""
     data, samplerate = sf.read(path)
     return data, samplerate
+
 
 def main():
     # wavファイルの読み込み
     path = 'ONSEI.wav'
     data, samplerate = wavload(path)
     fn = 436000    # データ整形のために指定
+
     # データを扱いやすく整形
     data = data[:fn]
     time = np.arange(0, len(data))/samplerate
     TOTAL_TIME = len(data)/samplerate
-
     WIDTH = 1000    # 分割フレームの大きさ
     OVERLAP = int(WIDTH / 2) # フレームのシフト幅
 
     wave_length = data.shape[0]    # 音声の全フレーム数
-    split_number = len(np.arange((WIDTH/2), wave_length, (WIDTH - OVERLAP))) # 音声の分割数
-
-    window = np.hamming(WIDTH)
-    pos = 0
+    split_number = len(np.arange((WIDTH/2), wave_length, (WIDTH - OVERLAP))) 
+    # 音声の分割数
 
     # 分割でフーリエ変換したデータのサイズを求める
-    fframe_size = np.fft.fft(data[pos:pos+WIDTH])
+    fframe_size = np.fft.fft(data[:WIDTH])
     fframe_size = fframe_size.shape[0]
     # STFTの格納配列
     spec = np.zeros([split_number, fframe_size], dtype=complex)
+
+    window = np.hamming(WIDTH) # ハミング窓
+    pos = 0 # 窓を掛ける位置
 
     # STFT
     for i in range(split_number):
@@ -57,6 +61,7 @@ def main():
         pos += OVERLAP
 
     
+    # 時間信号，スペクトログラム，逆変換後をグラフにプロット
     fig = plt.figure(figsize=(6,6))
     plt.rcParams["image.cmap"] = "jet"
     plt.rcParams["font.family"] = "Times New Roman"
@@ -71,7 +76,8 @@ def main():
 
     ax2 = fig.add_subplot(312)
     ax2.set_title("Spectrogram")
-    im = ax2.imshow(amp.T, extent=[0, TOTAL_TIME, 0, samplerate/2000], aspect='auto')
+    im = ax2.imshow(amp.T, extent=[0, TOTAL_TIME, 0, samplerate/2000]
+                    , aspect='auto')
     divider1 = make_axes_locatable(ax2)
     cax1 = divider1.append_axes("right", size="2%", pad=0.1)
     cbar = plt.colorbar(im, cax=cax1)
