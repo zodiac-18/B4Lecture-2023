523c1564733b41b4d8ea0cccde677ae678e84895
morita-koki <morita82007koki@gmail.com>
2023-04-28 20:50:49 +0900

    format by black

--- ex1/k_morita/spectrogram.py
+++ ex1/k_morita/spectrogram.py
@@ -2,22 +2,22 @@ import numpy as np
 
 
 def stft(y, sr, win_func=np.hamming, win_len=2048, ol=0.5):
-    window = win_func(win_len)                  # create window
-    hop_len = int(win_len * (1 - ol))           # length of a hop
-    n_steps = (len(y) - win_len) // hop_len     # number of fft steps
-    total_frame = hop_len * n_steps + win_len   # number of total frame
-    total_time = total_frame / sr               # total time [s]
+    window = win_func(win_len)  # create window
+    hop_len = int(win_len * (1 - ol))  # length of a hop
+    n_steps = (len(y) - win_len) // hop_len  # number of fft steps
+    total_frame = hop_len * n_steps + win_len  # number of total frame
+    total_time = total_frame / sr  # total time [s]
 
     # fft
     fft_array = [
-        np.fft.fft(window * y[i * hop_len: i * hop_len + win_len])
+        np.fft.fft(window * y[i * hop_len : i * hop_len + win_len])
         for i in range(n_steps)
     ]
     # convert list -> np.ndarray
     fft_array = np.array(fft_array).T
 
     # remove frequency element(from nyquist to samplerate)
-    fft_array = fft_array[fft_array.shape[0] // 2:]
+    fft_array = fft_array[fft_array.shape[0] // 2 :]
 
     return fft_array, total_time, total_frame
 
@@ -31,11 +31,11 @@ def istft(fft_array, sr, win_func=np.hamming, win_len=2048, ol=0.75):
 
     fft_array = fft_array.T
 
-    window = win_func(win_len)                   # create window
-    hop_len = int(win_len * (1 - ol))            # length of a hop
-    n_steps = len(fft_array)                     # number of fft steps
-    total_frame = hop_len * n_steps + win_len    # number of total frame
-    total_time = total_frame / sr                # total time [s]
+    window = win_func(win_len)  # create window
+    hop_len = int(win_len * (1 - ol))  # length of a hop
+    n_steps = len(fft_array)  # number of fft steps
+    total_frame = hop_len * n_steps + win_len  # number of total frame
+    total_time = total_frame / sr  # total time [s]
 
     # ifft
     ifft_array = [np.fft.ifft(fft) / window for fft in fft_array]
