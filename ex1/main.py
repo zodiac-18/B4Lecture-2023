f446f2f4cf24fc4bd2163db73eecfe8e9a84afe6
kenjiro ide <jollydog1230@gmail.com>
2023-04-29 12:05:54 +0900

    done

--- ex1/main.py
+++ ex1/main.py
@@ -16,36 +16,31 @@ def parse_args():
 def main():
     args = parse_args()
 
-    # Read input wav file
+    # 音声ファイルを読み込む
     rate, data = wavfile.read(args.input_file)
     data = np.array(data, dtype=float)
 
-    """波形をプロットする"""
+    # 波形をプロットする
     time = np.arange(0, len(data)) / rate
     plt.plot(time, data)
     plt.xlabel('Time [sec]')
     plt.ylabel('Amplitude')
     #plt.show()
-    
-    # STFT parameters
+
+    # STFTのそれぞれのパラメータ
     nfft = args.nfft
     hop_length = args.hop_length
     window = args.window
     window_func = signal.get_window(window, nfft)
-    print(f'window_func:{window_func}')
 
-    # Compute spectrogram
+    # スペクトログラムの計算
     spectrogram = np.zeros((1 + nfft // 2, (len(data) - nfft) // hop_length + 1), dtype=np.complex128)
-    print(f'spectrogram:{spectrogram}')
     for i in range(spectrogram.shape[1]):
         segment = data[i * hop_length:i * hop_length + nfft] * window_func
         spectrum = fftpack.fft(segment, n=nfft, axis=0)[:1 + nfft // 2]
         spectrogram[:, i] = spectrum
-    print(f'data:{data}')
-    print(f'segment:{segment}')
-    print(f'spectrum:{spectrum}')
-    print(f'spectrogram:{spectrogram}')
-    # Plot spectrogram
+
+    # スペクトログラムの描画
     plt.figure()
     plt.imshow(20 * np.log10(np.abs(spectrogram)), origin='lower', aspect='auto', cmap='jet')
     plt.xlabel('Time (s)')
@@ -54,21 +49,22 @@ def main():
     plt.title('Spectrogram')
     #plt.show()
 
-    import pdb; pdb.set_trace()
-
-    # Compute inverse STFT
-    data_inv = np.zeros_like(data)
-    window_sum = np.zeros_like(data_inv)
+    # 逆変換の計算
+    time_signal = np.zeros(len(data))
     for i in range(spectrogram.shape[1]):
         spectrum = spectrogram[:, i]
-        segment = fftpack.ifft(np.concatenate((spectrum, np.conj(spectrum[-2:0:-1]))), axis=0).real
-        segment = segment[:hop_length] if i == 0 else segment[hop_length:-hop_length]
-        data_inv[i * hop_length:i * hop_length + nfft] += segment
-        window_sum[i * hop_length:i * hop_length + nfft] += window_func ** 2
-    
-    data_inv /= window_sum
-    data_inv = np.int16(data_inv)
+        segment = fftpack.ifft(spectrum, n=nfft, axis=0)
+        segment = np.real(segment) * window_func
+        time_signal[i * hop_length:i * hop_length + nfft] += segment
 
+    # 逆変換した波形のプロット
+    plt.figure()
+    plt.plot(time, time_signal)
+    plt.xlabel('Time [sec]')
+    plt.ylabel('Amplitude')
+    plt.title('Inverse Transform')
+
+    plt.show()
 
 if __name__ == '__main__':
     main()
