4ee7c3c506e8af0097593e34c3b4f1e89556b314
ken-ogita <kenogita1811@gmail.com>
2023-05-05 02:42:28 +0900

    add main.py

---
+++ ex_1/k_ogita/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+import soundfile as sf
+
+def stft(data, framesize, overlap, window):
+    frame_length = data[0]
+    
+    
+    
+def main():
+    sound_file = 'ex1.wav'
+    framesize = 2048
+    overlap = 0.8
+    window = np.hamming(framesize)
+    data, samplerate = sf.read(sound_file)
+    time = len(data)/samplerate
+    stft_wave = stft(data, framesize)
+    
