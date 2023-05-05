9256fe9077dcc721ac8513fd8f31d901ee09d832
ken-ogita <kenogita1811@gmail.com>
2023-05-05 23:50:37 +0900

    add ex1.wav

--- ex_1/k_ogita/main.py
+++ ex_1/k_ogita/main.py
@@ -5,18 +5,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sf
 
-def stft(data, framesize, overlap, window):
-    frame_length = data[0]
+def stft(data, time, framesize, samplerate, overlap):
+    frame_cycle = framesize/samplerate
+    window = np.hamming(framesize)
+    split_time = int((time-frame_cycle*overlap)/frame_cycle*(1-overlap))
     
-    
+    pos = 0
+    for _ in range(split_time):
+        frame = np.fft.fft(data[pos:pos+framesize]*window)
+        stft_result.append(frame)
+        pos += framesize * (1 - overlap)
+    return stft_result
+
     
 def main():
     sound_file = 'ex1.wav'
     framesize = 2048
     overlap = 0.8
-    window = np.hamming(framesize)
     data, samplerate = sf.read(sound_file)
     time = len(data)/samplerate
     
-    stft_wave = stft(data, framesize, overlap, window)
+    stft_wave = stft(data, time, framesize, samplerate, overlap)
     
+    
+if "__main__" == __name__:
+    main()
