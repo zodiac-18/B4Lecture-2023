e1e0eeeefdfd56811bfa35a2135dc5e697bf905d
ken-ogita <kenogita1811@gmail.com>
2023-05-06 00:24:07 +0900

    add graph

--- ex_1/k_ogita/main.py
+++ ex_1/k_ogita/main.py
@@ -10,9 +10,10 @@ def stft(data, time, framesize, samplerate, overlap):
     window = np.hamming(framesize)
     split_time = int((time-frame_cycle*overlap)/frame_cycle*(1-overlap))
     
+    stft_result = []
     pos = 0
     for _ in range(split_time):
-        frame = np.fft.fft(data[pos:pos+framesize]*window)
+        frame = np.fft.fft(data[int(pos):int(pos+framesize)]*window)
         stft_result.append(frame)
         pos += framesize * (1 - overlap)
     return stft_result
@@ -27,6 +28,18 @@ def main():
     
     stft_wave = stft(data, time, framesize, samplerate, overlap)
     
+    fig = plt.figure()
+    ax1 = fig.add_subplot(3,1,1)
+    # 横軸
+    t = np.arange(0,len(data)) / samplerate
+    ax1.plot(t, data)
+    plt.title("Original signal")
+    plt.xlabel("Time[s]")
+    plt.ylabel("Magnitude")
+    plt.show()
+    plt.savefig("original.png")
+    plt.close()
+
     
 if "__main__" == __name__:
     main()
