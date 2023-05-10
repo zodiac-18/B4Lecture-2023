35580cf1c9686b99310942c786e8fe7d133670d5
shunsuke-iwashita <iwashita.shunsuke@g.sp.m.is.nagoya-u.ac.jp>
2023-05-10 23:12:24 +0900

    isort, black, flake8 を実行

--- ex1/s_iwashita/main.py
+++ ex1/s_iwashita/main.py
@@ -1,6 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import librosa
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def stft(x, fft_size, hop_size):
@@ -29,8 +29,9 @@ def istft(spec, fft_size, hop_size):
         x[start:end] += win * np.fft.irfft(spec[:, i], n=fft_size)
     return x
 
+
 # load a wav file
-filename = 'C:\\Users\\iwass\\Music\\iTunes\\iTunes Media\\Music\\Unknown Artist\\Unknown Album\\sample.wav'
+filename = "C:\\Users\\iwass\\Music\\iTunes\\iTunes Media\\Music\\Unknown Artist\\Unknown Album\\sample.wav"
 data, sr = librosa.load(filename, sr=None)
 
 # normalize
@@ -44,23 +45,29 @@ fig, axs = plt.subplots(3, 1, figsize=(6, 6))
 
 # display original signal
 axs[0].plot(x_t, data_norm)
-axs[0].set_title('Original signal')
-axs[0].set_ylabel('Magnitude')
-axs[0].set_xlim(0, len(data)/sr)
+axs[0].set_title("Original signal")
+axs[0].set_ylabel("Magnitude")
+axs[0].set_xlim(0, len(data) / sr)
 axs[0].set_ylim(-1, 1)
 
 # calculate the spectrogram
 n_fft = 256
 hop_length = n_fft // 4
-S = np.abs(stft(data_norm, fft_size=n_fft, hop_size=hop_length))**2
+S = np.abs(stft(data_norm, fft_size=n_fft, hop_size=hop_length)) ** 2
 
 # convert to logarithmic scale
 S_db = librosa.amplitude_to_db(S, ref=np.max)
 
 # display spectrogram
-axs[1].imshow(S_db, aspect='auto', origin='lower', cmap='jet', extent=[0, data_norm.shape[0]/sr, 0, sr/2/1000])
-axs[1].set_title('Spectrogram')
-axs[1].set_ylabel('Frequency (kHz)')
+axs[1].imshow(
+    S_db,
+    aspect="auto",
+    origin="lower",
+    cmap="jet",
+    extent=[0, data_norm.shape[0] / sr, 0, sr / 2 / 1000],
+)
+axs[1].set_title("Spectrogram")
+axs[1].set_ylabel("Frequency (kHz)")
 
 # restore waveform with inverse stft
 y_inv = istft(S, fft_size=n_fft, hop_size=hop_length)
@@ -73,10 +80,10 @@ y_t = np.arange(0, len(y_inv)) / sr
 
 # display re-synthesized signal
 axs[2].plot(y_t, y_inv_norm)
-axs[2].set_title('Re-synthesized signal')
-axs[2].set_ylabel('Magnitude')
-axs[2].set_xlabel('Time (s)')
-axs[2].set_xlim(0, len(data)/sr)
+axs[2].set_title("Re-synthesized signal")
+axs[2].set_ylabel("Magnitude")
+axs[2].set_xlabel("Time (s)")
+axs[2].set_xlim(0, len(data) / sr)
 axs[2].set_ylim(-1, 1)
 
 plt.tight_layout()
