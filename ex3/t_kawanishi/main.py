96e07f2fc28b0c3675c11ad068e2bf2337b471e3
980055617 <y980055617@gmail.com>
2023-05-14 19:42:16 +0900

    changed something

--- ex3/t_kawanishi/main.py
+++ ex3/t_kawanishi/main.py
@@ -17,20 +17,14 @@ def lsm_2(dataset: np.ndarray, deg: int) -> np.ndarray:
     Returns:
         np.ndarray: the coefficient of the function
     """
+    # example: argmin all y-(a1x**2 + a2x + a3)**2
+    # 1-degree -> 2-arg, 2-degree -> 3-arg
     #create matrix for lsm
-    A = []
-    B = []
-    for i in reversed(range(deg,deg*2+1)):
-        A_sub = []
-        for j in range(deg+1):
-            A_sub.append(np.sum(np.power(dataset[0],i-j)))
-        B.append(np.sum(np.power(dataset[0],i-deg)*dataset[1]))
-        A.append(A_sub)
-    A = np.array(A)
-    A = np.linalg.inv(A)
-    B = np.array(B)
+    A = np.array([np.sum(np.power(dataset[0], i-j)) for i in reversed(range(deg,deg*2+1)) for j in range(deg+1)]).reshape([deg+1,deg+1]) # maybe needed norm?
+    B = np.array([np.sum(np.power(dataset[0],i-deg)*dataset[1]) for i in reversed(range(deg,deg*2+1))])
+    A_inv = np.linalg.inv(A) # inverse of A
 
-    return (A@B)
+    return (A_inv@B)
 
 #least squares method 3-dimension
 def lsm_3(dataset: np.ndarray, deg: int) -> np.ndarray:
@@ -43,8 +37,19 @@ def lsm_3(dataset: np.ndarray, deg: int) -> np.ndarray:
     Returns:
         np.ndarray: the coefficient of the function
     """
-    pass
-
+    # example: argmin all z-(a1x**2 + a2y**2 + a3x + a4y + a5)**2
+    # 1-degree -> 3-arg, 2-degree -> 5-arg
+    # TODO: add function about create 3-dimension's least squares matrix
+    """
+    4,0  2,2  3,0  2,1  2,0
+    2,2  0,4  1,2  0,3  0,2
+    3,0  1,2  2,0  1,1  1,0
+    2,1  0,3  1,1  0,2  0,1
+    2,0  0,2  1,0  0,1  0,0
+    """
+    X = np.ravel(np.array([[i,0,0,i] for i in reversed(range(deg+1))]))
+    print(X+X)
+    A = np.array([np.sum(np.power(dataset[0], i)*np.power(dataset[1],j)) for i in range(deg+1)])
 
 
     return None
@@ -62,10 +67,15 @@ def genpoint(dataset: np.ndarray, function: np.ndarray, quant = 10000) -> tuple[
         tuple[np.ndarray, np.ndarray]: function data list for plot
                                         first is x-aixs and next is y-axis
     """
+    #decide plot range
     x_max = np.max(dataset[0])
     x_min = np.min(dataset[0])
+
+    #genereate point x
     x_group = np.linspace(x_min, x_max, quant)
     y_group = np.zeros(quant)
+
+    #generate point y
     for i in range(len(function)):
         y_group += np.power(x_group,i) * np.flipud(function)[i]
 
@@ -103,15 +113,22 @@ if __name__ == "__main__":
     #plot data
     if len(data) == 2:
         func = lsm_2(data, args.deg)
+        char = ""
+        char +=('{:.03f}'.format(func[args.deg]) + "x^" + str(args.deg))
+        for i in reversed(range(args.deg)):
+            if func[i] >0:
+                char += "+"
+            char +=(str('{:.03f}'.format(func[i])) + "x^" + str(i))
         x_group, y_group = genpoint(data,func)
         ax = plt.subplot()
         ax.plot(data[0],data[1],'.',label="dataset")
-        ax.plot(x_group,y_group,label="hi")
+        ax.plot(x_group,y_group,label=char)
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.legend(loc=0)
         plt.show()
     elif len(data) == 3:
+        lsm_3(data, args.deg)
         ax = plt.subplot(projection="3d")
         ax.plot(data[0],data[1],data[2],'.',c='b',label="dataset")
         ax.set_xlabel('X')
