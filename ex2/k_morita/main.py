43711f948808673ae8e41d457c65c9ab7a08d7d8
morita-koki <morita82007koki@gmail.com>
2023-05-17 05:28:58 +0900

    入力信号のスぺ区トログラムも表示するように変更

--- ex2/k_morita/main.py
+++ ex2/k_morita/main.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     mag_db = sp.mag_to_db(mag)
 
     # draw graph
-    fig, axes = plt.subplots(3, 1, figsize=(12, 8))
+    fig, axes = plt.subplots(4, 1, figsize=(12, 8))
     x = np.linspace(0, total_time, total_frame)
 
     # draw input wave
@@ -40,14 +40,33 @@ if __name__ == "__main__":
     axes[0].set_ylabel("Magnitude [dB]")
     axes[0].set_xlim(0, total_time)
 
-    axes[1].plot(x, filtered_y[:total_frame])
-    axes[1].set_title("filtered wave")
+    # draw input spectrogram
+    ifft, _, _ = sp.stft(y, samplerate, win_len=window_length, ol=overlap_rate)
+    imag, _ = sp.magphase(ifft)
+    im = axes[1].imshow(
+        sp.mag_to_db(imag),
+        cmap="jet",
+        aspect="auto",
+        vmin=-60,
+        vmax=30,
+        extent=[0, total_time, 0, nyquist_freq],
+    )
+    axes[1].set_title("spectrogram")
     axes[1].set_xlabel("Time [s]")
-    axes[1].set_ylabel("Magnitude [dB]")
+    axes[1].set_ylabel("Frequency [Hz]")
     axes[1].set_xlim(0, total_time)
+    axes[1].set_ylim(0, nyquist_freq)
+    plt.colorbar(mappable=im, ax=axes[1], orientation="horizontal")
+
+    # draw filtered wave
+    axes[2].plot(x, filtered_y[:total_frame])
+    axes[2].set_title("filtered wave")
+    axes[2].set_xlabel("Time [s]")
+    axes[2].set_ylabel("Magnitude [dB]")
+    axes[2].set_xlim(0, total_time)
 
     # draw spectrogram
-    im = axes[2].imshow(
+    im = axes[3].imshow(
         mag_db,
         cmap="jet",
         aspect="auto",
@@ -55,12 +74,12 @@ if __name__ == "__main__":
         vmax=30,
         extent=[0, total_time, 0, nyquist_freq],
     )
-    axes[2].set_title("spectrogram")
-    axes[2].set_xlabel("Time [s]")
-    axes[2].set_ylabel("Frequency [Hz]")
-    axes[2].set_xlim(0, total_time)
-    axes[2].set_ylim(0, nyquist_freq)
-    plt.colorbar(mappable=im, ax=axes[2], orientation="horizontal")
+    axes[3].set_title("spectrogram")
+    axes[3].set_xlabel("Time [s]")
+    axes[3].set_ylabel("Frequency [Hz]")
+    axes[3].set_xlim(0, total_time)
+    axes[3].set_ylim(0, nyquist_freq)
+    plt.colorbar(mappable=im, ax=axes[3], orientation="horizontal")
 
     plt.tight_layout()
     plt.savefig("result.png")
