6974bc78b88d84c25003503f4766dbc9187affe2
kenjiro ide <jollydog1230@gmail.com>
2023-05-22 14:10:22 +0900

    done

--- ex3/d_ide/linear_regression.py
+++ ex3/d_ide/linear_regression.py
@@ -1,26 +1,65 @@
+"""Linear regression file."""
 import numpy as np
 
+
 def simple_regression(x, N):
-    X = np.zeros((len(x) ,N+1))
+    """Calculate of single regression.
+
+    Args:
+        x (ndarray): data of x axis
+        N (int): dimension of x
+
+    Returns:
+        ndarray: matrix of X
+    """
+    X = np.zeros((len(x), N + 1))
 
     for i in range(0, len(x)):
-      for n in range(0, N+1):
-        X[i][n] = x[i]**n
+        for n in range(0, N + 1):
+            X[i][n] = x[i] ** n
 
     return X
 
 
 def beta_function(X, y):
+    """Calculate regression coefficients.
+
+    Args:
+        X (ndarray): data of x
+        y (ndarray): data of y
+
+    Returns:
+        ndarray: matrix of regression coefficients
+    """
     beta = np.linalg.inv(X.T @ X) @ X.T @ y
     return beta
 
 
 def regularization(X, y, lamb):
+    """Calculate regularization regression coefficients.
+
+    Args:
+        X (ndarray): data of x
+        y (ndarray): data of y
+        lamb (ndarray): parameter
+
+    Returns:
+        ndarray: matrix of regularization regression coefficients
+    """
     beta = np.linalg.inv(X.T @ X + lamb * np.identity(X.shape[1])) @ X.T @ y
     return beta
 
 
-def expect(beta, x):
+def prediction2d(beta, x):
+    """Predicting the equation of two-dimension.
+
+    Args:
+        beta (ndarray): regression coefficient
+        x (ndarray): data of x axis
+
+    Returns:
+        ndarray: predicted equation
+    """
     y = np.zeros_like(x)
     for i, b in enumerate(beta):
         y += b * x**i
@@ -29,23 +68,45 @@ def expect(beta, x):
 
 
 def multiple_regression(x, y, N1, N2):
+    """Calculate of multiple regression.
 
-    X = np.zeros((len(x) ,N1+N2+1))
+    Args:
+        x (ndarray): data of x axis
+        y (ndarray): data of y axis
+        N1 (int): dimension of x
+        N2 (int): dimension of y
+
+    Returns:
+        ndarray: matrix of X
+    """
+    X = np.zeros((len(x), N1 + N2 + 1))
 
     for i in range(0, len(x)):
-      for n in range(0, N1+1):
-        X[i][n] = x[i]**n
-      for k in range(1, N2+1):
-        X[i][k+N1] = y[i]**k
+        for n in range(0, N1 + 1):
+            X[i][n] = x[i] ** n
+        for k in range(1, N2 + 1):
+            X[i][k + N1] = y[i] ** k
 
     return X
 
 
-def expect2d(beta, x, y, N1, N2):
+def prediction3d(beta, x, y, N1, N2):
+    """Predicting the equation of two-dimension.
+
+    Args:
+        beta (ndarray): regression coefficient
+        x (ndarray): data of x axis
+        y (ndarray): data of y axis
+        N1 (int): dimension of x
+        N2 (int): dimension of y
+
+    Returns:
+        ndarray: predicted equation
+    """
     z = np.zeros_like(x)
-    for i in range(0, N1+1):
+    for i in range(0, N1 + 1):
         z += beta[i] * x**i
-    for i in range(1, N2+1):
-        z += beta[N1+i] * y**i
+    for i in range(1, N2 + 1):
+        z += beta[N1 + i] * y**i
 
     return z
--- ex3/d_ide/main.py
+++ ex3/d_ide/main.py
@@ -1,15 +1,15 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+"""Main file."""
 import argparse
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 import linear_regression as lr
 
 
 def parse_args():
     """Retrieve variables from the command prompt."""
-    parser = argparse.ArgumentParser(
-        description=""
-    )
+    parser = argparse.ArgumentParser(description="Perform regression analysis.")
     parser.add_argument(
         "--csv-file",
         type=str,
@@ -39,111 +39,139 @@ def parse_args():
 
 
 def open_csv(file_path):
-    data_set = np.loadtxt(
-        fname=file_path,
-        delimiter=",",
-        skiprows=1
-    )
+    """Read csv file.
+
+    Args:
+        file_path (str): Csv file to read
+
+    Returns:
+        ndarray: Data read
+    """
+    data_set = np.loadtxt(fname=file_path, delimiter=",", skiprows=1)
 
     return data_set
 
+
 def scat_plot2d(x, y, beta_r, beta):
+    """Plot two-dimensional data.
+
+    Args:
+        x (ndarray): data of x axis
+        y (ndarray): data of y axis
+        beta_r (ndarray): Regularized regression coefficient
+        beta (ndarray): regression coefficient
+    """
     x_line = np.linspace(min(x), max(x), 100)
-    y_expect = lr.expect(beta, x_line)
-    y_r = lr.expect(beta_r, x_line)
+    y_expect = lr.prediction2d(beta, x_line)
+    y_r = lr.prediction2d(beta_r, x_line)
     fig, ax = plt.subplots()
-    ax.scatter(x, y, facecolor="None", edgecolors="red", label="Observed")
-    ax.plot(x_line, y_expect, label=label1d(beta))
-    ax.plot(x_line, y_r, label=label1d(beta_r))
-    ax.set_title("Simple Linear-Regression")
+    ax.scatter(x, y, c="red", label="Original data")
+    ax.plot(x_line, y_expect, label=label2d(beta))
+    ax.plot(x_line, y_r, color="green", label=label2d(beta_r))
+    ax.set_title("Simple Linear Regression")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
-    ax.legend(loc=0)
-    fig.tight_layout()
+    ax.legend()
+    plt.savefig("result/simple_lr.png")
     plt.show()
 
 
 def scat_plot3d(x, y, z, beta, beta_r, N1, N2):
+    """Plot three-dimensional data.
+
+    Args:
+        x (ndarray): data of x axis
+        y (ndarray): data of y axis
+        z (ndarray): data of z axis
+        beta_r (ndarray): Regularized regression coefficient
+        beta (ndarray): regression coefficient
+        N1 (int) : dimension of x
+        N2 (int) : dimension of y
+    """
     x_line = np.linspace(min(x), max(x), 100)
     y_line = np.linspace(min(y), max(y), 100)
     x_mesh, y_mesh = np.meshgrid(x_line, y_line)
-    z_expect = lr.expect2d(beta, x_mesh, y_mesh, N1, N2)
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(111, projection="3d")
-    ax.set_xlabel("x", fontsize=22)
-    ax.set_ylabel("y", fontsize=22)
-    ax.set_zlabel("z", fontsize=22)
-    ax.scatter(x, y, z, marker="o", c="red", label="Observed data")
-    ax.plot_wireframe(
+    z_expect = lr.prediction3d(beta, x_mesh, y_mesh, N1, N2)
+    z_r = lr.prediction3d(beta_r, x_mesh, y_mesh, N1, N2)
+
+    fig = plt.figure(figsize=(10, 5))
+    ax1 = fig.add_subplot(1, 2, 1, projection="3d")
+    ax1.set_xlabel("x", fontsize=10)
+    ax1.set_ylabel("y", fontsize=10)
+    ax1.set_zlabel("z", fontsize=10)
+    ax1.set_title("Multiple Linear Regression")
+    ax1.scatter(x, y, z, marker="o", c="red", label="Original data")
+    ax1.plot_wireframe(
         x_mesh,
         y_mesh,
         z_expect,
         color="blue",
-        label=label2d(beta, 1, 2),
+        label=label3d(beta, N1),
     )
-    ax.legend()
-    plt.show()
+    ax1.legend()
 
-    z_r = lr.expect2d(beta_r, x_mesh, y_mesh, N1, N2)
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(111, projection="3d")
-    ax.set_xlabel("x", fontsize=22)
-    ax.set_ylabel("y", fontsize=22)
-    ax.set_zlabel("z", fontsize=22)
-    ax.scatter(x, y, z, marker="o", c="red", label="Observed data")
-    ax.plot_wireframe(
+    ax2 = fig.add_subplot(122, projection="3d")
+    ax2.set_xlabel("x", fontsize=10)
+    ax2.set_ylabel("y", fontsize=10)
+    ax2.set_zlabel("z", fontsize=10)
+    ax2.set_title("Multiple Linear Regression(regularization)")
+    ax2.scatter(x, y, z, marker="o", c="red", label="vOriginal data")
+    ax2.plot_wireframe(
         x_mesh,
         y_mesh,
         z_r,
-        color="blue",
-        label=label2d(beta_r, 1, 2),
+        color="green",
+        label=label3d(beta_r, N1),
     )
-    ax.legend()
+    ax2.legend()
+    plt.savefig("result/multiple_lr.png")
     plt.show()
 
 
-def label1d(beta):
-    """Generate label.
+def label2d(beta):
+    """Generate label of data1.csv and data2.csv.
+
     Args:
-        beta (np.ndarray): coefficient vector
+        beta (np.ndarray): regression coefficient
     Returns:
-        string: function label
+        string: function equation
     """
     equation = "y="
     for i, b in enumerate(beta):
         b = b[0]
         if i == 0:
-            equation += f'{b:+.3f}'
+            equation += f"{b:+.3f}"
         else:
-            equation += f'{b:+.3f}$x^{i}$'
+            equation += f"{b:+.3f}$x^{i}$"
     return equation
 
 
-def label2d(beta, N1, N2):
+def label3d(beta, N1):
     """Generate label.
+
     Args:
-        beta (np.ndarray): coefficient vector
-        N1 (int): dimension of x1
-        N2 (int): dimension of x2
+        beta (np.ndarray): regression coefficient
+        N1 (int): dimension of x axis
     Returns:
-        string: function label
+        string: function equation
     """
     equation = "y="
     for i, b in enumerate(beta):
-      b = beta[i]
-      b = b[0]
-      if i <= N1:
-        if i == 0:
-          equation += f'{b:.3f}'
+        b = beta[i]
+        b = b[0]
+        if i <= N1:
+            if i == 0:
+                equation += f"{b:.3f}"
+            else:
+                equation += f"{b:+.2f}$x^{i}$"
         else:
-          equation += f'{b:+.2f}$x^{i}$'
-      else:
-        equation += f'{b:+.2f}$y^{i}$'
+            equation += f"{b:+.2f}$y^{i}$"
 
     return equation
 
 
 def main():
+    """Regression analysis using the least squares method."""
     args = parse_args()
 
     file_path = args.csv_file
@@ -154,7 +182,7 @@ def main():
     data = open_csv(file_path)
 
     if data.shape[1] == 2:
-        x, y = data[: ,0], data[ : , 1]
+        x, y = data[:, 0], data[:, 1]
         y = y.reshape(-1, 1)
         X = lr.simple_regression(x, N1)
         beta = lr.beta_function(X, y)
@@ -162,12 +190,13 @@ def main():
         scat_plot2d(x, y, beta_r, beta)
 
     elif data.shape[1] == 3:
-        x, y, z = data[: ,0], data[ : , 1], data[: , 2]
+        x, y, z = data[:, 0], data[:, 1], data[:, 2]
         z = z.reshape(-1, 1)
-        X =lr.multiple_regression(x, y, N1, N2)
+        X = lr.multiple_regression(x, y, N1, N2)
         beta = lr.beta_function(X, z)
         beta_r = lr.regularization(X, z, lamb)
         scat_plot3d(x, y, z, beta, beta_r, N1, N2)
+
 
 if __name__ == "__main__":
     main()
