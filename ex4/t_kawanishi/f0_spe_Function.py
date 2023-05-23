f0a877a2d4e434fc03aabd32b0f9adb753f4ea87
980055617 <y980055617@gmail.com>
2023-05-23 19:52:40 +0900

    coding

--- ex4/t_kawanishi/f0_spe_Function.py
+++ ex4/t_kawanishi/f0_spe_Function.py
@@ -1,10 +1,10 @@
 """Fundamental frequency estimation function."""
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy
 
+
 # Autocorrelation function
-def ACF(data:np.ndarray) -> np.ndarray:
+def ACF(data: np.ndarray) -> np.ndarray:
     """To adapt autocorrelation function.
 
     Args:
@@ -15,14 +15,15 @@ def ACF(data:np.ndarray) -> np.ndarray:
     """
     length = len(data)
     result = np.zeros(length)
-    data_d = np.concatenate([data,data])
+    data_d = np.concatenate([data, data])
 
     for i in range(length):
-        result[i] = data@data_d[i:length+i]
+        result[i] = data @ data_d[i: length + i]
     return result
 
+
 # peak
-def peak(result:np.ndarray) -> int:
+def peak(result: np.ndarray) -> int:
     """search the peak of the input
 
     Args:
@@ -31,40 +32,44 @@ def peak(result:np.ndarray) -> int:
     Returns:
         int: the peak of the input
     """
-    peak = np.zeros(len(result)-2)
-    for i in range(len(result)-2):
-        if result[i] < result[i+1] and result[i+1] > result[i+2]:
-            peak[i] =result[i+1]
+    peak = np.zeros(len(result) - 2)
+    for i in range(len(result) - 2):
+        if result[i] < result[i + 1] and result[i + 1] > result[i + 2]:
+            peak[i] = result[i + 1]
     f0 = np.argmax(peak)
     return f0
 
+
 # estimating f0 with Autocorrelation Function
-def f0_ACF(data:np.ndarray,sample_rate:int,size=100,overlap_r= 0.5) -> np.ndarray:
+def f0_ACF(
+        data: np.ndarray, sample_rate: int,
+        size=100, overlap_r=0.5) -> np.ndarray:
     """To adapt autocorrelation Function for estimate f0.
 
     Args:
         data (np.ndarray): sound data
         sample_rate (int): sound's sample rate
         size (int, optional): size of frame. Defaults to 100.
-        overlap_r (float, optional): overlap rate between frame and frame. Defaults to 0.5.
+        overlap_r (float, optional): overlap rate between
+                                    frame and frame. Defaults to 0.5.
 
     Returns:
         np.ndarray: fundamental group
     """
-    #generate var for compute
-    dis = int(size*(1-overlap_r))
-    f_num = int(len(data) // (dis)) #amount of the frame
-    f0_g = np.zeros(f_num) #Fundamental frequency group
+    # generate var for compute
+    dis = int(size * (1 - overlap_r))
+    f_num = int(len(data) // (dis))  # amount of the frame
+    f0_g = np.zeros(f_num)  # Fundamental frequency group
     win = np.hamming(size)
 
-    #search each frame's f0
+    # search each frame's f0
     for i in range(f_num):
         if i * dis + size > len(data):
             break
-        #ACF return
-        result = ACF(data[i*dis:i*dis+size]*win)
+        # ACF return
+        result = ACF(data[i * dis: i * dis + size] * win)
 
-        #peak group
+        # peak group
         f0 = peak(result)
         if f0 == 0:
             f0_g[i] = 0
@@ -72,8 +77,9 @@ def f0_ACF(data:np.ndarray,sample_rate:int,size=100,overlap_r= 0.5) -> np.ndarra
             f0_g[i] = sample_rate / f0
     return f0_g
 
+
 # cepstrum method
-def cepstrum(data:np.ndarray) -> np.ndarray:
+def cepstrum(data: np.ndarray) -> np.ndarray:
     """_summary_
 
     Args:
@@ -88,12 +94,15 @@ def cepstrum(data:np.ndarray) -> np.ndarray:
     # log
     power_spec = np.log10(np.abs(fft_data))
 
-    #ifft
+    # ifft
     cep = np.real(np.fft.irfft(power_spec))
     return cep
 
+
 # estimating f0 with cepstrum method
-def f0_cep(data:np.ndarray, sample_rate:int, lif=50, size=100, overlap_r = 0.5)->np.ndarray:
+def f0_cep(
+    data: np.ndarray, sample_rate: int, lif=50, size=100, overlap_r=0.5
+) -> np.ndarray:
     """To adapt cepstrum method for estimate f0.
 
     Args:
@@ -101,30 +110,32 @@ def f0_cep(data:np.ndarray, sample_rate:int, lif=50, size=100, overlap_r = 0.5)-
         sample_rate (int): sound's sample rate
         lif (int, optional): the lifter range.Defaults to 50
         size (int, optional): size of frame. Defaults to 100.
-        overlap_r (float, optional): overlap rate between frame and frame. Defaults to 0.5.
+        overlap_r (float, optional): overlap rate between frame and frame.
+                                     Defaults to 0.5.
 
     Returns:
         np.ndarray: fundamental group
     """
-    #generate var for compute
-    dis = int(size*(1-overlap_r))
-    f_num = int(len(data) // (dis)) #amount of the frame
-    f0_g = np.zeros(f_num) #Fundamental frequency group
+    # generate var for compute
+    dis = int(size * (1 - overlap_r))
+    f_num = int(len(data) // (dis))  # amount of the frame
+    f0_g = np.zeros(f_num)  # Fundamental frequency group
     win = np.hamming(size)
 
-    #search each frame's f0
+    # search each frame's f0
     for i in range(f_num):
         if i * dis + size > len(data):
             break
 
-        #adapt to cepstrum
-        result = cepstrum(data[i*dis:i*dis+size]*win)
+        # adapt to cepstrum
+        result = cepstrum(data[i * dis: i * dis + size] * win)
 
         f0_g[i] = peak(result[lif: (len(result) // 2)])
-    return sample_rate / (f0_g +lif)
+    return sample_rate / (f0_g + lif)
+
 
 # levinson durbin algorithm
-def levinson(r:np.ndarray, deg:int) -> tuple[np.ndarray,np.ndarray]:
+def levinson(r: np.ndarray, deg: int) -> tuple[np.ndarray, np.ndarray]:
     """Levinson durbin algorithm
 
     Args:
@@ -138,8 +149,8 @@ def levinson(r:np.ndarray, deg:int) -> tuple[np.ndarray,np.ndarray]:
         a = np.array([1, -r[1] / r[0]])
         E = a.dot(r[:2])
     else:
-        a_s, E_s = levinson(r, deg-1)
-        k_s = -(a_s@r[deg:0:-1]) / E_s
+        a_s, E_s = levinson(r, deg - 1)
+        k_s = -(a_s @ r[deg:0:-1]) / E_s
         U = np.append(a_s, 0)
         V = U[::-1]
         a = U + k_s * V
@@ -147,15 +158,17 @@ def levinson(r:np.ndarray, deg:int) -> tuple[np.ndarray,np.ndarray]:
 
     return a, E
 
+
 # lpc method
-def lpc(data:np.ndarray, deg:int, size=100) ->np.ndarray:
+def lpc(data: np.ndarray, deg: int, size=100) -> np.ndarray:
     r = ACF(data)
-    a, e = levinson(r[: len(r) // 2],deg)
-    w, h = scipy.signal.freqz(np.sqrt(e),a,size,"whole")
+    a, e = levinson(r[: len(r) // 2], deg)
+    w, h = scipy.signal.freqz(np.sqrt(e), a, size, "whole")
     return 20 * np.log10(np.abs(h))
 
+
 # use cepstrum to estimate spectral envelop
-def cep_env(data:np.ndarray, lif=50) -> np.ndarray:
+def cep_env(data: np.ndarray, lif=50) -> np.ndarray:
     """To adapt cepstrum method for estimate spectrum envelop.
 
     Args:
@@ -166,9 +179,10 @@ def cep_env(data:np.ndarray, lif=50) -> np.ndarray:
         np.ndarray: fundamental group
     """
     cep = cepstrum(data)
-    cep[lif : -lif] = 0
+    cep[lif:-lif] = 0
 
     return 20 * np.real(np.fft.fft(cep))
+
 
 if __name__ == "__main__":
     pass
--- ex4/t_kawanishi/main.py
+++ ex4/t_kawanishi/main.py
@@ -3,16 +3,19 @@
 import argparse
 import re
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 import ex1Function as ex1
 import f0_spe_Function as f0
 
 if __name__ == "__main__":
-    #create parser
-    parser = argparse.ArgumentParser(description="This is a program to estimate sound's Fundamental frequency and spectral envelop.")
-    parser.add_argument("path",help="path of the sound")
+    # create parser
+    parser = argparse.ArgumentParser(
+        description="""This is a program to estimate sound's
+                    Fundamental frequency and spectral envelop."""
+    )
+    parser.add_argument("path", help="path of the sound")
     parser.add_argument(
         "-s",
         "--f_size",
@@ -21,10 +24,15 @@ if __name__ == "__main__":
         type=int,
     )
     parser.add_argument(
-        "-r", "--overlap_r", help="overlap rate between 0 to 1", default=0.5, type=float
+        "-r", "--overlap_r", help="overlap rate between 0 to 1",
+        default=0.5, type=float
     )
     parser.add_argument(
-        "-l", "--y_limit", help="limit of spectrogram's frequency", default=20000, type=int
+        "-l",
+        "--y_limit",
+        help="limit of spectrogram's frequency",
+        default=20000,
+        type=int,
     )
     parser.add_argument(
         "-d",
@@ -33,47 +41,62 @@ if __name__ == "__main__":
         default=5,
         type=int,
     )
-    #get arguments
-    args =parser.parse_args()
+    # get arguments
+    args = parser.parse_args()
 
-    #get sound file
+    # get sound file
     data, s_rate = ex1.load_sound(args.path)
 
-    #adapt to Autocorrelation
-    f0_g1= f0.f0_ACF(data,s_rate,size=args.f_size,overlap_r=args.overlap_r)
+    # adapt to Autocorrelation
+    f0_g1 = f0.f0_ACF(data, s_rate, size=args.f_size, overlap_r=args.overlap_r)
 
-    #adapt to spectrum
-    f0_g2 = f0.f0_cep(data,s_rate,lif=50,size=args.f_size,overlap_r=args.overlap_r)
+    # adapt to spectrum
+    f0_g2 = f0.f0_cep(data, s_rate, lif=50, size=args.f_size,
+                      overlap_r=args.overlap_r)
 
-    #create figure and sub figure
-    fig = plt.figure(figsize = (15,10))
+    # create figure and sub figure
+    fig = plt.figure(figsize=(15, 10))
     ax1 = fig.add_subplot(1, 2, 1)
     ax2 = fig.add_subplot(1, 2, 2)
 
-    #create spectrum
-    ex1.show_spectrogram(data, ax1,Fs=args.f_size,overlap_r=args.overlap_r,sample_rate=s_rate,y_lim=args.y_limit)
-    ex1.show_spectrogram(data, ax2,Fs=args.f_size,overlap_r=args.overlap_r,sample_rate=s_rate,y_lim=args.y_limit)
+    # create spectrum
+    ex1.show_spectrogram(
+        data,
+        ax1,
+        Fs=args.f_size,
+        overlap_r=args.overlap_r,
+        sample_rate=s_rate,
+        y_lim=args.y_limit,
+    )
+    ex1.show_spectrogram(
+        data,
+        ax2,
+        Fs=args.f_size,
+        overlap_r=args.overlap_r,
+        sample_rate=s_rate,
+        y_lim=args.y_limit,
+    )
 
     # create file name
-    name = re.sub(r".+\\","",args.path)
-    name = re.sub(r"\..+","",name)
+    name = re.sub(r".+\\", "", args.path)
+    name = re.sub(r"\..+", "", name)
 
     # plot f0 generate by autocorrelation Function
-    dis = int(args.f_size*args.overlap_r)
-    frame_l = int(len(data) // dis)*dis
+    dis = int(args.f_size * args.overlap_r)
+    frame_l = int(len(data) // dis) * dis
     x_t = np.arange(0, frame_l, dis)
     x_t = x_t / s_rate
-    ax1.plot(x_t,f0_g1)
+    ax1.plot(x_t, f0_g1)
     ax1.set_title("spectrum and f0 estimation with autocorrelation")
 
-    #plot f0 generate by cepstrum method
-    ax2.plot(x_t,f0_g2)
+    # plot f0 generate by cepstrum method
+    ax2.plot(x_t, f0_g2)
     ax2.set_title("spectrum and f0 estimation with cepstrum")
     plt.savefig("f0_" + name)
 
     # spectrum envelop
     win = np.hamming(args.f_size)
-    win_data = data[:args.f_size] * win
+    win_data = data[: args.f_size] * win
 
     log = 20 * np.log10(np.abs(np.fft.rfft(win_data)))
     cep = f0.cep_env(win_data, lif=50)
@@ -82,8 +105,8 @@ if __name__ == "__main__":
     plt.figure()
     f_group = np.fft.rfftfreq(args.f_size)
     plt.plot(f_group, log, label="Spectrum")
-    plt.plot(f_group, cep[:len(log)], label="Cepstrum")
-    plt.plot(f_group, lpc[:len(log)], label="LPC")
+    plt.plot(f_group, cep[: len(log)], label="Cepstrum")
+    plt.plot(f_group, lpc[: len(log)], label="LPC")
     plt.xlabel("Frequency[Hz]")
     plt.ylabel("Log amplitude spectrum[dB]")
     plt.legend()
