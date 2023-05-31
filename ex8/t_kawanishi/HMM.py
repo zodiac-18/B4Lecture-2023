9ff19f79d4d2f8d69be5017c383b4a209353d129
980055617 <y980055617@gmail.com>
2023-06-01 02:41:51 +0900

    final version

--- ex8/t_kawanishi/HMM.py
+++ ex8/t_kawanishi/HMM.py
@@ -1,15 +1,143 @@
 """Function of HMM."""
-
 import pickle
+import re
+import time
 
-def load_pickle(path:str)->dict:
-    """load pickle file
+import numpy as np
 
-    Args:
-        path (str): path to the pickle
 
-    Returns:
-        dict: pickle contents
-    """
-    data = pickle.load(open(path, "rb"))
-    return data
+class HMM:
+    def __init__(self, path):
+        self.data = self.load_pickle(path)
+        self.fname = self.get_fname(path)
+        self.answer = np.array(self.data["answer_models"])
+        self.output = np.array(self.data["output"])
+        self.PI = np.array(self.data["models"]["PI"])
+        self.A = np.array(self.data["models"]["A"])
+        self.B = np.array(self.data["models"]["B"])
+        if np.all(np.tril(self.A, k=-1) == 0):
+            self.type = "left-to-right HMM"
+        else:
+            self.type = "Ergodic HMM"
+
+    def get_fname(self, path: str) -> str:
+        """Get file name.
+
+        Args:
+            path (str): path to the input file
+
+        Returns:
+            str: file name
+        """
+        f_name = re.sub(r".+\\", "", path)
+        f_name = re.sub(r"\..+", "", f_name)
+        return f_name
+
+    def get_info(self):
+        """get pickle information."""
+        print()
+        print((self.fname + "   " + self.type).center(40, "="))
+        print("answer models of each sequence: "
+              + str(self.answer.shape[0]).rjust(4))
+        print("number of sequences: " + str(self.output.shape[0]).rjust(15))
+        print("length of each sequence: "
+              + str(self.output.shape[1]).rjust(11))
+        print("Number of models: " + str(self.PI.shape[0]).rjust(18))
+        print("Number of states: " + str(self.PI.shape[1]).rjust(18))
+        print("Number of output states: " + str(self.B.shape[2]).rjust(11))
+        print()
+
+    def load_pickle(self, path: str) -> dict:
+        """load pickle file
+
+        Args:
+            path (str): path to the pickle
+
+        Returns:
+            dict: pickle contents
+        """
+        data = pickle.load(open(path, "rb"))
+        return data
+
+    def Forward(self) -> tuple[np.ndarray, float, float]:
+        """predict HMM by forward
+
+        Returns:
+            np.ndarray: predict
+            float: 1 for-loop times
+            float: 2 for-loop times
+        """
+
+        # ===============only used 1 for-loop==============
+        start = time.perf_counter()
+        output = self.output[:, :, np.newaxis]
+        # initialize stage
+        # shape is (models_n, status_n, sequence_n, 1)
+        alpha = self.PI[:, :, np.newaxis] * self.B[:, :, output[:, 0]]
+
+        # loop stage
+        for i in range(1, self.output.shape[1]):
+            alpha = (
+                np.sum(alpha * self.A[:, :, np.newaxis], axis=1)
+                .transpose(0, 2, 1)[:, :, :, np.newaxis]
+                * self.B[:, :, output[:, i]]
+            )
+
+        pre = np.argmax(np.sum(alpha, axis=1), axis=0).reshape(-1)
+        t1 = time.perf_counter() - start
+
+        # ================used 2 for-loop====================
+        start = time.perf_counter()
+        pre1 = np.zeros_like(self.answer)
+        for i in range(self.output.shape[0]):
+            alpha = self.PI * self.B[:, :, output[i, 0]]
+            for j in range(self.output.shape[1]):
+                alpha = (
+                    np.sum(alpha * self.A, axis=1)[:, :, np.newaxis]
+                    * self.B[:, :, output[i, j]]
+                )
+            pre1[i] = np.argmax(np.sum(alpha, axis=1))
+        t2 = time.perf_counter() - start
+
+        return pre, t1 * 1000, t2 * 1000
+
+    def Viterbi(self) -> tuple[np.ndarray, float, float]:
+        """predict HMM by viterbi
+
+        Returns:
+            np.ndarray: predict
+            float: 1 for-loop times
+            float: 2 for-loop times
+        """
+        # ===============only used 1 for-loop==============
+        start = time.perf_counter()
+        output = self.output[:, :, np.newaxis]
+        # initialize stage
+        # shape is (models_n, status_n, sequence_n, 1)
+        alpha = self.PI[:, :, np.newaxis] * self.B[:, :, output[:, 0]]
+
+        # loop stage
+        for i in range(1, self.output.shape[1]):
+            alpha = (
+                np.max(alpha * self.A[:, :, np.newaxis], axis=1)
+                .transpose(0, 2, 1)[:, :, :, np.newaxis]
+                * self.B[:, :, output[:, i]]
+            )
+
+        pre = np.argmax(np.max(alpha, axis=1), axis=0).reshape(-1)
+        t1 = time.perf_counter() - start
+
+        # ================used 2 for-loop====================
+        start = time.perf_counter()
+        pre1 = np.zeros_like(self.answer)
+        for i in range(self.output.shape[0]):
+            alpha = self.PI * self.B[:, :, output[i, 0]]
+            for j in range(self.output.shape[1]):
+                alpha = (
+                    np.max(alpha * self.A, axis=1)[:, :, np.newaxis]
+                    * self.B[:, :, output[i, j]]
+                )
+            pre1[i] = np.argmax(np.max(alpha, axis=1))
+        t2 = time.perf_counter() - start
+
+        return pre, t1 * 1000, t2 * 1000
--- ex8/t_kawanishi/main.py
+++ ex8/t_kawanishi/main.py
@@ -1,36 +1,80 @@
 """To adapt HMM."""
 import argparse
-import re
 
+import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
 
 import HMM
+
 
 def main():
     # create parser
     parser = argparse.ArgumentParser(
-        description="This is a program to model prediction by HMM")
-    parser.add_argument("path",help="path to the file")
+        description="This is a program to model prediction by HMM"
+    )
+    parser.add_argument("path", help="path to the file")
 
     # get arguments
     args = parser.parse_args()
 
-    # create file name
-    f_name = re.sub(r".+\\","",args.path)
-    f_name = re.sub(r"\..+","",f_name)
-
     # read out pickle
     """
     data
-        answer_models (100)
+        answer_models (100,)
         output (100,100)
         models
             PI (5,3,1) or (5,5,1)
             A (5,3,3) or (5,5,5)
             B (5,3,5) or (5,5,5)
     """
-    data = HMM.load_pickle(args.path)
-    print(np.array(data["models"]['A']))
+    data = HMM.HMM(args.path)
+    data.get_info()
+    preF, tF1, tF2 = data.Forward()
+    preV, tV1, tV2 = data.Viterbi()
+    cmF = confusion_matrix(data.answer, preF)
+    cmV = confusion_matrix(data.answer, preV)
+    Acu_F = np.trace(cmF) / np.sum(cmF)
+    Acu_V = np.trace(cmV) / np.sum(cmV)
+
+    # plot confusion matrix
+    fig = plt.figure(figsize=(16, 9))
+    plt.suptitle(data.fname
+                 + " learning outcome\nData type: "
+                 + data.type, fontsize=20)
+    A = fig.add_subplot(1, 2, 1)
+    B = fig.add_subplot(1, 2, 2)
+    sns.heatmap(cmF, ax=A, cmap="Purples", annot=True, square=True)
+    sns.heatmap(cmV, ax=B, cmap="Purples", annot=True, square=True)
+    A.set_title(
+        "Forward "
+        + data.fname
+        + "\nAccuracy: "
+        + "{:.3f}".format(Acu_F)
+        + "\n1 for-loop time: "
+        + "{:.3f}".format(tF1)
+        + "ms     2 for-loop time: "
+        + "{:.3f}".format(tF2)
+        + "ms"
+    )
+    B.set_title(
+        "Viterbi "
+        + data.fname
+        + "\nAccuracy: "
+        + "{:.3f}".format(Acu_V)
+        + "\n1 for-loop time: "
+        + "{:.3f}".format(tV1)
+        + "ms     2 for-loop time: "
+        + "{:.3f}".format(tV2)
+        + "ms"
+    )
+    A.set_xlabel("predicted models", fontsize=16)
+    B.set_xlabel("predicted models", fontsize=16)
+    A.set_ylabel("answer models", fontsize=16)
+    B.set_ylabel("answer models", fontsize=16)
+    plt.savefig(data.fname + "_confusion_matrix.png")
+    plt.show()
 
 
 if __name__ == "__main__":
