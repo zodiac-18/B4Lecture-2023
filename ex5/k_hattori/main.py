62fe1fb15233fefea3701fa26de090ca5bd37e8e
Kimihiro Hattori <hattori.kimihiro@g.sp.m.is.nagoya-u.ac.jp>
2023-06-01 22:05:54 +0900

    add function

--- ex5/k_hattori/main.py
+++ ex5/k_hattori/main.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import csv
 import argparse
 import random
+from scipy.fftpack import dct
+import myfunc as mf
 
 
 def load_csv(path):
@@ -15,15 +17,191 @@ def load_csv(path):
 
     return array
 
-def k_means(data, k):
-    rand_index = random.sample(data.shape[0], k)
-    centroids = data[rand_index]
+
+def clustering_2d(data, k, centroids):
+    label = np.zeros(data.shape[0])
     for i in range(data.shape[0]):
-        distance = np.zeros(k)
-        distance = np.linalg.norm(data[i] - centroids)
+        distance = centroids - data[i]
+        distance = distance[:,0]** 2 + distance[:, 1]** 2
+        label[i] = np.argmin(distance)
+    return label
 
 
+def k_means_2d(data, k):
+    rand_index = random.sample(range(data.shape[0]), k)
+    centroids = data[rand_index]
+    min_e = 0.03
+    count = 0
+    error = 1000
+    while(error > min_e and count < 1000):
+        label = clustering_2d(data, k, centroids)
+        previous_cent = centroids.copy()
+        for i in range(k):
+            N = len(data[label==i])
+            centroids[i,0] = np.mean(data[label==i, 0])
+            centroids[i,1] = np.mean(data[label==i, 1])
+        difference = np.linalg.norm(np.abs(centroids - previous_cent), axis=1)
+        error = difference[np.argmax(difference)]
+        count += 1
 
+    label = clustering_2d(data, k, centroids)
+    for i in range(k):
+        plt.scatter(data[label==i, 0], data[label==i, 1])
+    plt.show()
+
+    return label
+
+
+def clustering_3d(data, k, centroids):
+    label = np.zeros(data.shape[0])
+    for i in range(data.shape[0]):
+        distance = centroids - data[i]
+        distance = distance[:,0]** 2 + distance[:, 1]** 2 + distance[:, 2]** 2
+        label[i] = np.argmin(distance)
+    return label
+
+
+def k_means_3d(data, k):
+    rand_index = random.sample(range(data.shape[0]), k)
+    centroids = data[rand_index]
+    min_e = 0.03
+    count = 0
+    error = 1000
+    while(error > min_e and count < 1000):
+        label = clustering_3d(data, k, centroids)
+        previous_cent = centroids.copy()
+        for i in range(k):
+            N = len(data[label==i])
+            centroids[i,0] = np.mean(data[label==i, 0])
+            centroids[i,1] = np.mean(data[label==i, 1])
+            centroids[i,2] = np.mean(data[label==i, 2])
+        difference = np.linalg.norm(np.abs(centroids - previous_cent), axis=1)
+        error = difference[np.argmax(difference)]
+        count += 1
+
+    label = clustering_3d(data, k, centroids)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    print(data.shape)
+    for i in range(k):
+        ax.scatter(data[label==i, 0], data[label==i, 1], data[label==i, 2])
+    plt.show()
+
+    return label
+
+
+def hz2mel(f):
+    return 2595 * np.log(f / 700.0 + 1.0)
+
+
+def mel2hz(m):
+    return 700 * (np.exp(m / 2595) - 1.0)
+
+
+def melFilterBank(channel, size, samplerate):
+    # Nyquist rate (frequency, mel and index)
+    fmax = samplerate / 2
+    melmax = hz2mel(fmax)
+    nmax = size // 2
+    # Frequency resolution
+    delta_f = samplerate / size
+    # mel width per channel
+    delta_mel = melmax / (channel + 1)
+    # Center frequency of filter
+    centers_mel = np.arange(1, channel + 1) * delta_mel
+    centers_freq = mel2hz(centers_mel)
+    centers_index = np.round(centers_freq / delta_f)
+    # Index of the start and stop position of each filter
+    index_start = np.hstack(([0], centers_index[0: channel - 1]))
+    index_stop = np.hstack((centers_index[0: channel], [nmax]))
+    # create melfilterbank
+    filterbank = np.zeros([channel, nmax])
+    for i in range(0, channel):
+        increment = 1.0 / (centers_index[i] - index_start[i])
+        for j in range(int(index_start[i]), int(centers_index[i])):
+            filterbank[i, j] = (j - index_start[i]) * increment
+        decrement = 1.0 / (index_stop[i] - centers_index[i])
+        for j in range(int(centers_index[i]), int(index_stop[i])):
+            filterbank[i, j] = 1.0 - ((j - centers_index[i]) * decrement)
+
+    return filterbank, centers_freq
+
+
+def MFCC(data, length, samplerate):
+    order = 12
+    N = len(data) // length
+    hanning = np.hanning(length)
+    mfcc = np.zeros([N, order])
+    pos = 0
+    for i in range(N):
+        segment = data[pos: pos+length]
+        fft_seg = np.log10(np.abs(np.fft.fft(segment)))
+        channel = 20
+        filterbank, centers_f = melFilterBank(channel, len(fft_seg), samplerate)
+        m_seg = np.dot(fft_seg[:len(fft_seg)//2], filterbank.T)
+        ceps = dct(m_seg, type=2, norm='ortho', axis=-1)
+        mfcc[i] = ceps[:order]
+        pos += length
+
+    return mfcc
+
+
+def delta_MFCC(mfcc):
+    d_mfcc = np.zeros(mfcc.shape)
+    mfcc = np.append(mfcc, np.zeros([1, mfcc.shape[1]]), axis=0)
+    for i in range(d_mfcc.shape[0]):
+        d_mfcc[i] = mfcc[i+1] - mfcc[i]
+
+    return d_mfcc
+
+
+def MFCC_plot(data, mfcc, d_mfcc, dd_mfcc, TOTAL_TIME, samplerate):
+    freq = np.linspace(0, samplerate / 2, data.shape[1] // 2)
+    amp = np.abs(data[:, data.shape[1] // 2 - 1:: -1])
+    amp = 20 * np.log10(amp)
+
+    plt.rcParams["image.cmap"] = "nipy_spectral"
+    plt.rcParams["font.family"] = "Times New Roman"
+    plt.rcParams["font.size"] = 10
+
+    fig = plt.figure(figsize=[8,8])
+
+    plt.subplot(411)
+    plt.title("Spectrogram")
+    plt.imshow(amp.T[freq<=8000, :], extent=[0, TOTAL_TIME, 0, 8000], aspect="auto")
+    plt.colorbar()
+    plt.xlim(0, TOTAL_TIME)
+    plt.ylim(0, 8000)
+    plt.ylabel("Frequency [Hz]")
+
+    plt.subplot(412)
+    plt.title("MFCC sequence")
+    plt.imshow(mfcc.T[::-1], extent=[0, TOTAL_TIME, 0, 12], aspect="auto")
+    plt.colorbar()
+    plt.xlim(0, TOTAL_TIME)
+    plt.ylim(0, 12)
+    plt.ylabel("MFCC")
+
+    plt.subplot(413)
+    plt.title("ΔMFCC sequence")
+    plt.imshow(d_mfcc.T[::-1], extent=[0, TOTAL_TIME, 0, 12], aspect="auto")
+    plt.colorbar()
+    plt.xlim(0, TOTAL_TIME)
+    plt.ylim(0, 12)
+    plt.ylabel("ΔMFCC")
+
+    plt.subplot(414)
+    plt.title("ΔΔMFCC sequence")
+    plt.imshow(dd_mfcc.T[::-1], extent=[0, TOTAL_TIME, 0, 12], aspect="auto")
+    plt.colorbar()
+    plt.xlim(0, TOTAL_TIME)
+    plt.ylim(0, 12)
+    plt.xlabel("Time [s]")
+    plt.ylabel("ΔΔMFCC")
+
+
+    plt.tight_layout()
+    plt.show()
 
 
 def main():
@@ -43,6 +221,26 @@ def main():
     # parse arguments
     args = parser.parse_args()
 
+    path = args.filename
+    if path.find('.csv') != -1:
+        data = load_csv(path)
+        k = args.k
+        dimension = np.shape(data)[1]
+
+        if dimension == 2:
+            label = k_means_2d(data, k)
+        elif dimension == 3:
+            label = k_means_3d(data, k)
+
+    elif path.find('.wav') != -1:
+        data, samplerate = mf.wavload(path)
+        TOTAL_TIME = len(data) / samplerate
+
+        spec = mf.STFT(data, 1024)
+        mfcc = MFCC(data, 2048, samplerate)
+        d_mfcc = delta_MFCC(mfcc)
+        dd_mfcc = delta_MFCC(d_mfcc)
+        MFCC_plot(spec, mfcc, d_mfcc, dd_mfcc, TOTAL_TIME, samplerate)
 
 if __name__ == "__main__":
     main()
