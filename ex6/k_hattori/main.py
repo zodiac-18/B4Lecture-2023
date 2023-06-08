65fcf98c09ba90271e49f73ca0c6ca3364e8b70d
Kimihiro Hattori <hattori.kimihiro@g.sp.m.is.nagoya-u.ac.jp>
2023-06-08 12:54:08 +0900

    complete

--- ex6/k_hattori/main.py
+++ ex6/k_hattori/main.py
@@ -1,7 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import csv
+"""Read files and perform PCA."""
 import argparse
+import csv
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def load_csv(path):
@@ -25,31 +27,47 @@ def load_csv(path):
 
 
 def PCA(data):
+    """
+    Perform PCA to find contribution rate and projection matrix.
+
+    Args:
+        data (ndarray): Target array.
+
+    Returns:
+        ndarray: Contriburtion rate array.
+        ndarray: Projection matrix.
+    """
     dimension = data.shape[1]
     std_data = np.zeros(data.shape)
     # Standardization of data
     for i in range(dimension):
         # Means and standard deviations
-        mu = np.mean(data[:,i])
-        sigma = np.std(data[:,i])
+        mu = np.mean(data[:, i])
+        sigma = np.std(data[:, i])
         std_data[:, i] = (data[:, i] - mu) / sigma
     # Calculate covariance
-    cov = np.cov(np.array([std_data[:,i] for i in range(dimension)]))
+    cov = np.cov(np.array([std_data[:, i] for i in range(dimension)]))
     # Eigenvalues and eigenvectors
     eig_value, eig_vector = np.linalg.eig(cov)
     sort_eig = np.sort(eig_value)[::-1]
     j = 0
     sort_vector = np.zeros(eig_vector.shape)
     for i in sort_eig:
-        sort_vector[:, j] = np.ravel(eig_vector[:,eig_value == i])
+        sort_vector[:, j] = np.ravel(eig_vector[:, eig_value == i])
         j += 1
     # Calculation of Contribution Ratio
-    cont =  sort_eig / np.sum(sort_eig)
-        
+    cont = sort_eig / np.sum(sort_eig)
+
     return cont, sort_vector
 
 
 def main():
+    """
+    Perform PCA and plot data.
+
+    Returns:
+        None
+    """
     # make parser
     parser = argparse.ArgumentParser(
         prog="main.py",
@@ -80,8 +98,13 @@ def main():
     if dimension == 2:
         str1 = f"Contribution rate:{Cont_rate[0]:.3f}"
         str2 = f"Contribution rate:{Cont_rate[1]:.3f}"
-        plt.scatter(data[:, 0], data[:, 1], edgecolors="magenta",
-                    facecolor="None", label="data1")
+        plt.scatter(
+            data[:, 0],
+            data[:, 1],
+            edgecolors="magenta",
+            facecolor="None",
+            label="data1",
+        )
         plt.axline((0, 0), W[:, 0], color="blue", label=str1)
         plt.axline((0, 0), W[:, 1], color="red", label=str2)
         plt.xlabel("x1")
@@ -103,8 +126,14 @@ def main():
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
-        ax.scatter(data[:, 0], data[:, 1], data[:, 2], edgecolors="magenta",
-                   facecolor="None", label="data2")
+        ax.scatter(
+            data[:, 0],
+            data[:, 1],
+            data[:, 2],
+            edgecolors="magenta",
+            facecolor="None",
+            label="data2",
+        )
         ax.plot(x, a_yx[0] * x, a_zx[0] * x, color="blue", label=str1)
         ax.plot(x, a_yx[1] * x, a_zx[1] * x, color="red", label=str2)
         ax.plot(x, a_yx[2] * x, a_zx[2] * x, color="green", label=str3)
@@ -115,8 +144,13 @@ def main():
         plt.tight_layout()
         plt.show()
 
-        plt.scatter(data_2d[:, 0], data_2d[:, 1], edgecolor="magenta",
-                    facecolor="None", label="data2 in 2D")
+        plt.scatter(
+            data_2d[:, 0],
+            data_2d[:, 1],
+            edgecolor="magenta",
+            facecolor="None",
+            label="data2 in 2D",
+        )
         plt.xlim(-3, 3)
         plt.ylim(-1, 1)
         plt.xlabel("x1")
@@ -127,7 +161,9 @@ def main():
 
     else:
         # Total contribution rate
-        total_cont = np.array([np.sum(Cont_rate[:i]) for i in range(len(Cont_rate))])
+        total_cont = np.array(
+            [np.sum(Cont_rate[:i]) for i in range(len(Cont_rate))]
+            )
         print("Order:", np.min(np.where(total_cont >= 0.9)))
         plt.plot(total_cont, color="blue", label="Total contribution rate")
         plt.axline((0, 0.9), (1, 0.9), color="red", label="Threshold 90%")
