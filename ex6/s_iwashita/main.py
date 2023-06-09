60a42d6f09226c7dd96ed236a64fef894363256c
shunsuke-iwashita <iwashita.shunsuke@g.sp.m.is.nagoya-u.ac.jp>
2023-06-10 01:01:15 +0900

    update main.py

--- ex6/s_iwashita/main.py
+++ ex6/s_iwashita/main.py
@@ -1,25 +1,22 @@
+"""Perform principal component analysis."""
 import argparse
 
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats
-import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
+
 class PCA:
+    """This is a class associated with PCA."""
 
     def __init__(self, filename):
         self.filename = filename
-        self.data_ss = None
-        self.n_components = None
-        self.standardized = None
-        self.eig = None
-        self.eig_vec = None
-        self.data_tf = None
 
     def fit(self):
-        #
+        """Perform PCA:"""
         self.data = np.loadtxt(self.filename, delimiter=",")
-        #
+        # Standardize data
         self.data_ss = scipy.stats.zscore(self.data)
         # get n_components
         self.n_components = len(self.data[0])
@@ -36,9 +33,11 @@ class PCA:
         self.contribution_rate = self.eig / np.sum(self.eig)
 
     def transform(self):
+        """Transform data"""
         self.data_tf = np.dot(self.data, self.w)
 
     def plot_scatter(self):
+        """Generate a scatter plot."""
         if self.n_components == 2 or self.n_components == 3:
             x_min = int(np.min(self.data[:, 0]) // 1)
             x_max = int(np.max(self.data[:, 0]) // 1 + 1)
@@ -50,32 +49,108 @@ class PCA:
             fig = plt.figure()
             if self.n_components == 2:
                 ax = fig.add_subplot(111)
-                ax.scatter(self.data[:, 0], self.data[:, 1], c="None", edgecolor="magenta", label="{}".format(self.filename[3:8]))
-                ax.plot(x, x * slope_xy[0], c="b", label="Contribution rate: {:.3f}".format(self.contribution_rate[0]))
-                ax.plot(x, x * slope_xy[1], c="g", label="Contribution rate: {:.3f}".format(self.contribution_rate[1]))
-                ax.set(title="{}".format(self.filename[3:8]), xlabel="$x_1$", ylabel="$x_2$", xlim=(x_min, x_max), ylim=(y_min, y_max), xticks=range(x_min, x_max + 1, 1), yticks=range(y_min, y_max + 1, 1))
+                ax.scatter(
+                    self.data[:, 0],
+                    self.data[:, 1],
+                    c="None",
+                    edgecolor="magenta",
+                    label="{}".format(self.filename[3:8]),
+                )
+                ax.plot(
+                    x,
+                    x * slope_xy[0],
+                    c="b",
+                    label="Contribution rate: {:.3f}".format(self.contribution_rate[0]),
+                )
+                ax.plot(
+                    x,
+                    x * slope_xy[1],
+                    c="g",
+                    label="Contribution rate: {:.3f}".format(self.contribution_rate[1]),
+                )
+                ax.set(
+                    title="{}".format(self.filename[3:8]),
+                    xlabel="$x_1$",
+                    ylabel="$x_2$",
+                    xlim=(x_min, x_max),
+                    ylim=(y_min, y_max),
+                )
+                ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+                ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
             elif self.n_components == 3:
                 z_min = int(np.min(self.data[:, 2]) // 1)
                 z_max = int(np.max(self.data[:, 2]) // 1 + 1)
                 slope_xz = self.eig_vec[2] / self.eig_vec[0]
                 ax = fig.add_subplot(111, projection="3d")
-                ax.scatter(self.data[:, 0], self.data[:, 1], self.data[:, 2], c="None", edgecolor="magenta", label="{}".format(self.filename[3:8]))
-                ax.plot(x, x * slope_xy[0], x * slope_xz[0], c="b", label="Contribution rate: {:.3f}".format(self.contribution_rate[0]))
-                ax.plot(x, x * slope_xy[1], x * slope_xz[1], c="g", label="Contribution rate: {:.3f}".format(self.contribution_rate[1]))
-                ax.plot(x, x * slope_xy[2], x * slope_xz[2], c="r", label="Contribution rate: {:.3f}".format(self.contribution_rate[2]))
-                ax.set(title="{}".format(self.filename[3:8]), xlabel="$x_1$", ylabel="$x_2$", zlabel="$x_3$", xlim=(x_min, x_max), ylim=(y_min, y_max), zlim=(z_min, z_max), xticks=range(x_min, x_max + 1, 1), yticks=range(y_min, y_max + 1, 1), zticks=range(z_min, z_max + 1, 1))
+                ax.scatter(
+                    self.data[:, 0],
+                    self.data[:, 1],
+                    self.data[:, 2],
+                    c="None",
+                    edgecolor="magenta",
+                    label="{}".format(self.filename[3:8]),
+                )
+                ax.plot(
+                    x,
+                    x * slope_xy[0],
+                    x * slope_xz[0],
+                    c="b",
+                    label="Contribution rate: {:.3f}".format(self.contribution_rate[0]),
+                )
+                ax.plot(
+                    x,
+                    x * slope_xy[1],
+                    x * slope_xz[1],
+                    c="g",
+                    label="Contribution rate: {:.3f}".format(self.contribution_rate[1]),
+                )
+                ax.plot(
+                    x,
+                    x * slope_xy[2],
+                    x * slope_xz[2],
+                    c="r",
+                    label="Contribution rate: {:.3f}".format(self.contribution_rate[2]),
+                )
+                ax.set(
+                    title="{}".format(self.filename[3:8]),
+                    xlabel="$x_1$",
+                    ylabel="$x_2$",
+                    zlabel="$x_3$",
+                    xlim=(x_min, x_max),
+                    ylim=(y_min, y_max),
+                    zlim=(z_min, z_max),
+                )
+                ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+                ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+                ax.zaxis.set_major_locator(MaxNLocator(integer=True))
             ax.grid(ls="--")
             ax.legend(loc="upper left")
             plt.savefig("result/{}.png".format(self.filename[3:8]))
             plt.close()
 
     def plot_cumulative_contribution_rate(self):
+        """Calculate and plot cumulative contribution rate."""
         self.cum_con_rate = np.cumsum(self.contribution_rate)
+        self.index_above_90 = np.min(np.where(self.cum_con_rate >= 0.9))
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.plot(self.cum_con_rate, c="b", label="Cumulative contribution rate")
-        ax.set(title="{}".format(self.filename[3:8]), xlabel="Index of contribution rate", ylabel="Cumulative contribution rate", yticks=[0, 0.2, 0.4, 0.6, 0.8, 1])
+        ax.plot(
+            self.index_above_90, self.cum_con_rate[self.index_above_90], "D-", c="g"
+        )
+        ax.axline(
+            (self.index_above_90, 0),
+            (self.index_above_90, self.cum_con_rate[self.index_above_90]),
+            c="g",
+            label="{}".format(self.index_above_90),
+        )
+        ax.set(
+            title="{}".format(self.filename[3:8]),
+            xlabel="Index of contribution rate",
+            ylabel="Cumulative contribution rate",
+            yticks=[0, 0.2, 0.4, 0.6, 0.8, 1],
+        )
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         ax.axhline(y=0.9, c="r", label="Line of 90%")
         ax.grid()
@@ -84,47 +159,61 @@ class PCA:
         plt.close()
 
     def dimensional_compression(self):
-        idx_compression = np.min(np.where(self.cum_con_rate >= 0.9)) + 1
-        if idx_compression == 2 or idx_compression == 3:
+        """Perform dimensional compression."""
+        compression_dim = self.index_above_90 + 1
+        if compression_dim == 2 or compression_dim == 3:
             x_min = int(np.min(self.data_tf[:, 0]) // 1)
             x_max = int(np.max(self.data_tf[:, 0]) // 1 + 1)
             y_min = int(np.min(self.data_tf[:, 1]) // 1)
             y_max = int(np.max(self.data_tf[:, 1]) // 1 + 1)
             fig = plt.figure()
-            if idx_compression == 2:
+            if compression_dim == 2:
                 ax = fig.add_subplot(111)
-                ax.scatter(self.data_tf[:, 0], self.data_tf[:, 1], c="None", edgecolor="magenta")
-                ax.set(title="Comperssed {}".format(self.filename[3:8]), xlabel="$x_1$", ylabel="$x_2$", xlim=(x_min, x_max), ylim=(y_min, y_max), xticks=range(x_min, x_max + 1, 1), yticks=range(y_min, y_max + 1, 1))
-            elif idx_compression == 3:
+                ax.scatter(
+                    self.data_tf[:, 0],
+                    self.data_tf[:, 1],
+                    c="None",
+                    edgecolor="magenta",
+                )
+                ax.set(
+                    title="Comperssed {}".format(self.filename[3:8]),
+                    xlabel="$x_1$",
+                    ylabel="$x_2$",
+                    xlim=(x_min, x_max),
+                    ylim=(y_min, y_max),
+                )
+                ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+                ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+            elif compression_dim == 3:
                 z_min = int(np.min(self.data_tf[:, 2]) // 1)
                 z_max = int(np.max(self.data_tf[:, 2]) // 1 + 1)
                 ax = fig.add_subplot(111, projection="3d")
-                ax.scatter(self.data_tf[:, 0], self.data_tf[:, 1], self.data_tf[:, 2], c="None", edgecolor="magenta")
-                ax.set(title="Comperssed {}".format(self.filename[3:8]), xlabel="$x_1$", ylabel="$x_2$", zlabel="$x_3$", xlim=(x_min, x_max), ylim=(y_min, y_max), zlim=(z_min, z_max), xticks=range(x_min, x_max + 1, 1), yticks=range(y_min, y_max + 1, 1), zticks=range(z_min, z_max + 1, 1))
+                ax.scatter(
+                    self.data_tf[:, 0],
+                    self.data_tf[:, 1],
+                    self.data_tf[:, 2],
+                    c="None",
+                    edgecolor="magenta",
+                )
+                ax.set(
+                    title="Comperssed {}".format(self.filename[3:8]),
+                    xlabel="$x_1$",
+                    ylabel="$x_2$",
+                    zlabel="$x_3$",
+                    xlim=(x_min, x_max),
+                    ylim=(y_min, y_max),
+                    zlim=(z_min, z_max),
+                )
+                ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+                ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+                ax.zaxis.set_major_locator(MaxNLocator(integer=True))
             ax.grid(ls="--")
             plt.savefig("result/{}_compressed.png".format(self.filename[3:8]))
-            plt.close
-
-
-"""
-d=2:
-    scatter
-    plot
-
-d=3:
-    scatter 
-    plot
-    compress
-    contribution rate
-
-d>4:
-    contribution rate
-    n_compress <= 3:
-        compress
-"""
+            plt.close()
 
 
 def main():
+    """Perform PCA."""
     parser = argparse.ArgumentParser(description="This program performs PCA.")
 
     parser.add_argument("-f", "--filename", help="FIle name for PCA")
@@ -139,6 +228,7 @@ def main():
     pca.plot_scatter()
     pca.plot_cumulative_contribution_rate()
     pca.dimensional_compression()
+
 
 if __name__ == "__main__":
     main()
