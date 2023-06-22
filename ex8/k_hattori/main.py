4a89757971998fe2bcf4b200507815362b69bcf2
Kimihiro Hattori <hattori.kimihiro@g.sp.m.is.nagoya-u.ac.jp>
2023-06-22 11:54:36 +0900

    add main function

--- ex8/k_hattori/main.py
+++ ex8/k_hattori/main.py
@@ -5,6 +5,7 @@ from sklearn.metrics import confusion_matrix
 import pandas as pd
 import seaborn as sns
 import argparse
+import time
 
 
 def load_pickle(path):
@@ -24,12 +25,11 @@ def forward_algorithm(output, init_dist, trans_prob, out_prob):
     for i in range(output.shape[0]):
         alpha = init_dist[:, :, 0] * out_prob[:, :, output[i, 0]]
         for j in range(1, output.shape[1]):
-            alpha = np.sum(trans_prob * alpha[:, :, None],
-                           axis=1) * out_prob[:, :, output[i, j]]
+            alpha = np.sum(trans_prob * alpha[:, :, None], axis=1) * out_prob[:, :, output[i, j]]
         # select the model with the highest probability of generation
         predict[i] = np.argmax(np.sum(alpha, axis=1))
 
-        return predict
+    return predict
 
 
 def viterbi_algorithm(output, init_dist, trans_prob, out_prob):
@@ -38,12 +38,11 @@ def viterbi_algorithm(output, init_dist, trans_prob, out_prob):
     for i in range(output.shape[0]):
         delta = init_dist[:, :, 0] * out_prob[:, :, output[i, 0]]
         for j in range(1, output.shape[1]):
-            delta = np.max(trans_prob * delta[:, :, None],
-                           axis=1) * out_prob[:, :, output[i, j]]
+            delta = np.max(trans_prob * delta[:, :, None], axis=1) * out_prob[:, :, output[i, j]]
         # select the model with the highest probability
         predict[i] = np.argmax(np.max(delta, axis=1))
 
-        return predict
+    return predict
 
 
 def cm_plot(predict, answer, algorithm):
@@ -54,14 +53,51 @@ def cm_plot(predict, answer, algorithm):
     # calculate accuracy
     acc = np.sum(predict - answer == 0) / len(answer) * 100
     # plot confusion matrix
-    sns.heatmap(cm, annot=True, cbar=False, cmap="binary")
+    sns.heatmap(cm, annot=True, cbar=False, cmap="Reds")
     plt.title(algorithm + f"\n(Accuracy : {acc}%)")
     plt.xlabel("Predicted model")
     plt.ylabel("Actual model")
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="main.py",
+        usage="Demonstration of argparser",
+        description="description",
+        epilog="end",
+        add_help=True,
+    )
+    # add arguments
+    parser.add_argument("-f", dest="filename", help="Filename", required=True)
+    # parse arguments
+    args = parser.parse_args()
+    path = args.filename
+
+    # load pickle data
+    output, answer_models, init_dist, trans_prob, out_prob = load_pickle(path)
+
+    # execute algorithms and measure execution times
+    forward_start = time.perf_counter()
+    forward_pred = forward_algorithm(output, init_dist, trans_prob, out_prob)
+    forward_stop = time.perf_counter()
+    viterbi_start = time.perf_counter()
+    viterbi_pred = viterbi_algorithm(output, init_dist, trans_prob, out_prob)
+    viterbi_stop = time.perf_counter()
+
+    forward_time = forward_stop - forward_start
+    viterbi_time = viterbi_stop - viterbi_start
+
+    plt.rcParams["font.family"] = "Times New Roman"
+    plt.rcParams["font.size"] = 12
+    # display results
+    print(f"Execution time of forward algorithm : {forward_time:.5f} [s]")
+    print(f"Execution time of viterbi algorithm : {viterbi_time:.5f} [s]")
+    plt.figure(figsize=(11, 5))
+    plt.subplot(121)
+    cm_plot(forward_pred, answer_models, "Forward algorithm")
+    plt.subplot(122)
+    cm_plot(viterbi_pred, answer_models, "Viterbi algorithm")
+    plt.show()
 
 
 if __name__ == "__main__":
